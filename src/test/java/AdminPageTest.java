19c2a0af29421bf8e7fe61dcac5d1a4367bdbbd4
ELENAQQQ <kuzheleva.elena@mail.ru>
2023-03-19 15:34:52 +0100

    BeforeTest with log-in at AdminPageTest

--- src/test/java/AdminPageTest.java
+++ src/test/java/AdminPageTest.java
@@ -1,2 +1,40 @@
-public class AdminPageTest extends BasePageTest {
+import io.restassured.response.Response;
+import org.testng.Assert;
+import pageobject.BasePage;
+import org.testng.annotations.*;
+import static io.restassured.RestAssured.*;
+import static Config.Config.*;
+import static Config.Credentials.*;
+
+public class AdminPageTest extends BasePage {
+
+    private String accessToken;
+
+    @BeforeTest
+    public void start() {
+        baseURI = BASE_URI;
+
+        String body = "{\n" +
+                " \"email\": \"" + ADMIN_EMAIL + "\",\n" +
+                " \"password\": \"" + ADMIN_PASSWORD + "\"\n" +
+                "}";
+
+        Response response = given()
+                .header("Content-Type", "application/json")
+                .body(body)
+                .post("jwt/create/");
+
+        response.then().log().all().statusCode(200);
+
+        setAccessToken(response.then().extract().response().jsonPath().getString("access"));
+    }
+
+    public String getAccessToken() {
+        return accessToken;
+    }
+
+    public void setAccessToken(String accessToken){
+        this.accessToken = accessToken;
+    }
+
 }
