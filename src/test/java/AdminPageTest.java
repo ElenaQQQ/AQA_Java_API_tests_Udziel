34512e414902d3879e1af51ac0ddf4cb5186a042
ELENAQQQ <kuzheleva.elena@mail.ru>
2023-03-19 19:16:10 +0100

    AdminPageTest - get users list. User instance

--- src/test/java/AdminPageTest.java
+++ src/test/java/AdminPageTest.java
@@ -1,7 +1,12 @@
+import entities.User;
+import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import org.testng.Assert;
 import pageobject.BasePage;
 import org.testng.annotations.*;
+
+import java.util.List;
+
 import static io.restassured.RestAssured.*;
 import static Config.Config.*;
 import static Config.Credentials.*;
@@ -35,6 +40,20 @@ public class AdminPageTest extends BasePage {
 
     public void setAccessToken(String accessToken){
         this.accessToken = accessToken;
+    }
+
+    @Test
+    public void getUsersList(){
+        List<User> users = given()
+                .header("Authorization", "Token " + getAccessToken())
+                .when()
+                .contentType(ContentType.JSON)
+                .get("users/")
+                .then().log().all()
+                .extract().jsonPath().getList(".", User.class);
+//        System.out.println("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX Users list length = " + users.size());
+        Assert.assertTrue(users.size() > 0,
+                "Don't get users list");
     }
 
 }
