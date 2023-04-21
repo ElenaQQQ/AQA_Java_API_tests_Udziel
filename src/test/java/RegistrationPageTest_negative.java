a7abdb0f95a1f020798999e2bcef3606813493d5
ELENAQQQ <kuzheleva.elena@mail.ru>
2023-04-21 18:28:53 +0200

    registration tests negative: refactor to set userEmail as random, userName and userPassword at the beginning

--- src/test/java/RegistrationPageTest_negative.java
+++ src/test/java/RegistrationPageTest_negative.java
@@ -7,14 +7,14 @@ import pageobject.RegistrationPage;
 import static Config.TestData.*;
 
 public class RegistrationPageTest_negative  extends RegistrationPage {
-    public String userEmail;
-    public String userPassword;
-    public String userName;
+    public String userEmail = USER_RANDOM_EMAIL;
+    public String userPassword = USER_TEST_PASSWORD;
+    public String userName = USER_TEST_NAME;
 
     @Test (description = "with invalid e-mail: empty field")
     public void userRegistration_7(){
         userEmail = USER_EMAIL7;
-        UserDataToRegistration userDataToRegistration = new UserDataToRegistration(userEmail, USER_TEST_PASSWORD, USER_TEST_NAME);
+        UserDataToRegistration userDataToRegistration = new UserDataToRegistration(userEmail, userPassword, userName);
         Response response = registerUser(userDataToRegistration);
 
         response.then().log().all().statusCode(400);
@@ -26,7 +26,7 @@ public class RegistrationPageTest_negative  extends RegistrationPage {
     @Test (description = "with invalid e-mail: 255 symbols ")
     public void userRegistration_8(){
         userEmail = USER_EMAIL8;
-        UserDataToRegistration userDataToRegistration = new UserDataToRegistration(userEmail, USER_TEST_PASSWORD, USER_TEST_NAME);
+        UserDataToRegistration userDataToRegistration = new UserDataToRegistration(userEmail, userPassword, userName);
         Response response = registerUser(userDataToRegistration);
 
         response.then().log().all().statusCode(400);
@@ -39,7 +39,7 @@ public class RegistrationPageTest_negative  extends RegistrationPage {
     public void userRegistration_9(){
 
         userEmail = USER_EMAIL9;
-        UserDataToRegistration userDataToRegistration = new UserDataToRegistration(userEmail, USER_TEST_PASSWORD, USER_TEST_NAME);
+        UserDataToRegistration userDataToRegistration = new UserDataToRegistration(userEmail, userPassword, userName);
         Response response = registerUser(userDataToRegistration);
 
         response.then().log().all().statusCode(400);
@@ -51,7 +51,7 @@ public class RegistrationPageTest_negative  extends RegistrationPage {
     public void userRegistration_10(){
 
         userEmail = USER_EMAIL10;
-        UserDataToRegistration userDataToRegistration = new UserDataToRegistration(userEmail, USER_TEST_PASSWORD, USER_TEST_NAME);
+        UserDataToRegistration userDataToRegistration = new UserDataToRegistration(userEmail, userPassword, userName);
         Response response = registerUser(userDataToRegistration);
 
         response.then().log().all().statusCode(400);
@@ -64,7 +64,7 @@ public class RegistrationPageTest_negative  extends RegistrationPage {
     public void userRegistration_11(){
 
         userEmail = USER_EMAIL11;
-        UserDataToRegistration userDataToRegistration = new UserDataToRegistration(userEmail, USER_TEST_PASSWORD, USER_TEST_NAME);
+        UserDataToRegistration userDataToRegistration = new UserDataToRegistration(userEmail, userPassword, userName);
         Response response = registerUser(userDataToRegistration);
 
         response.then().log().all().statusCode(400);
@@ -77,7 +77,7 @@ public class RegistrationPageTest_negative  extends RegistrationPage {
     public void userRegistration_12(){
 
         userEmail = USER_EMAIL12;
-        UserDataToRegistration userDataToRegistration = new UserDataToRegistration(userEmail, USER_TEST_PASSWORD, USER_TEST_NAME);
+        UserDataToRegistration userDataToRegistration = new UserDataToRegistration(userEmail, userPassword, userName);
         Response response = registerUser(userDataToRegistration);
 
         response.then().log().all().statusCode(400);
@@ -89,7 +89,7 @@ public class RegistrationPageTest_negative  extends RegistrationPage {
     @Test (description = "with invalid password: 7 symbols")
     public void userRegistration_21(){
         userPassword = USER_PASSWORD21;
-        UserDataToRegistration userDataToRegistration = new UserDataToRegistration(USER_EMAIL1, userPassword, USER_TEST_NAME);
+        UserDataToRegistration userDataToRegistration = new UserDataToRegistration(userEmail, userPassword, userName);
         Response response = registerUser(userDataToRegistration);
 
         response.then().log().all().statusCode(400);
@@ -101,7 +101,7 @@ public class RegistrationPageTest_negative  extends RegistrationPage {
     @Test (description = "with invalid password: equal to name + \"1\"")
     public void userRegistration_22(){
         userPassword = USER_PASSWORD22;
-        UserDataToRegistration userDataToRegistration = new UserDataToRegistration(USER_EMAIL1, userPassword, USER_TEST_NAME);
+        UserDataToRegistration userDataToRegistration = new UserDataToRegistration(userEmail, userPassword, userName);
         Response response = registerUser(userDataToRegistration);
 
         response.then().log().all().statusCode(400);
@@ -113,7 +113,7 @@ public class RegistrationPageTest_negative  extends RegistrationPage {
     @Test (description = "with invalid password: equal to email")
     public void userRegistration_23(){
         userPassword = USER_PASSWORD23;
-        UserDataToRegistration userDataToRegistration = new UserDataToRegistration(USER_EMAIL1, userPassword, USER_TEST_NAME);
+        UserDataToRegistration userDataToRegistration = new UserDataToRegistration(userEmail, userPassword, userName);
         Response response = registerUser(userDataToRegistration);
 
         response.then().log().all().statusCode(400);
@@ -125,7 +125,7 @@ public class RegistrationPageTest_negative  extends RegistrationPage {
     @Test (description = "with invalid password: numbers only >= 8 symbols")
     public void userRegistration_24(){
         userPassword = USER_PASSWORD24;
-        UserDataToRegistration userDataToRegistration = new UserDataToRegistration(USER_EMAIL1, userPassword, USER_TEST_NAME);
+        UserDataToRegistration userDataToRegistration = new UserDataToRegistration(userEmail, userPassword, userName);
         Response response = registerUser(userDataToRegistration);
 
         response.then().log().all().statusCode(400);
@@ -137,7 +137,7 @@ public class RegistrationPageTest_negative  extends RegistrationPage {
     @Test (description = "with invalid password: qwerty")
     public void userRegistration_25(){
         userPassword = USER_PASSWORD25;
-        UserDataToRegistration userDataToRegistration = new UserDataToRegistration(USER_EMAIL1, userPassword, USER_TEST_NAME);
+        UserDataToRegistration userDataToRegistration = new UserDataToRegistration(userEmail, userPassword, userName);
         Response response = registerUser(userDataToRegistration);
 
         response.then().log().all().statusCode(400);
@@ -149,7 +149,7 @@ public class RegistrationPageTest_negative  extends RegistrationPage {
     @Test (description = "with invalid name: empty field")
     public void userRegistration_31(){
         userName = USER_NAME31;
-        UserDataToRegistration userDataToRegistration = new UserDataToRegistration(USER_EMAIL1, USER_TEST_PASSWORD, userName);
+        UserDataToRegistration userDataToRegistration = new UserDataToRegistration(userEmail, userPassword, userName);
         Response response = registerUser(userDataToRegistration);
 
         response.then().log().all().statusCode(400);
@@ -161,7 +161,7 @@ public class RegistrationPageTest_negative  extends RegistrationPage {
     @Test (description = "with invalid name: 151 symbol")
     public void userRegistration_32(){
         userName = USER_NAME32;
-        UserDataToRegistration userDataToRegistration = new UserDataToRegistration(USER_EMAIL1, USER_TEST_PASSWORD, userName);
+        UserDataToRegistration userDataToRegistration = new UserDataToRegistration(userEmail, userPassword, userName);
         Response response = registerUser(userDataToRegistration);
 
         response.then().log().all().statusCode(400);
@@ -173,7 +173,7 @@ public class RegistrationPageTest_negative  extends RegistrationPage {
     @Test (description = "with invalid name: contains \"*/#\"")
     public void userRegistration_33(){
         userName = USER_NAME33;
-        UserDataToRegistration userDataToRegistration = new UserDataToRegistration(USER_EMAIL1, USER_TEST_PASSWORD, userName);
+        UserDataToRegistration userDataToRegistration = new UserDataToRegistration(userEmail, userPassword, userName);
         Response response = registerUser(userDataToRegistration);
 
         response.then().log().all().statusCode(400);
@@ -181,7 +181,6 @@ public class RegistrationPageTest_negative  extends RegistrationPage {
                 "TEST FAILED: No expected message");
 
     }
-
 
 
 }
