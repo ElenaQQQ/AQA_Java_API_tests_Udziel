3f9e36f7c8ade5c5e93138ebd5c268523006f20a
ELENAQQQ <kuzheleva.elena@mail.ru>
2023-05-26 16:03:13 +0200

    reduce all users entities to one

--- src/main/java/pageobject/LoginPage.java
+++ src/main/java/pageobject/LoginPage.java
@@ -1,4 +1,19 @@
 package pageobject;
 
+import org.testng.annotations.DataProvider;
+
 public class LoginPage extends BasePage {
+
+    /*@DataProvider(name = "Locators for tests 41-51")
+    public static Object[][] getData(){
+        return new Object[][]{
+                {"41", footerExLabLogo},
+                {"42", footerCopywrite},
+                {"51", footerEmail},
+                {"45", footerInstagramLink},
+                {"43", footerLinkedinLink},
+                {"47", footerTelegramLink},
+                {"49", footerYoutubeLink}
+        };
+    }*/
 }
--- src/test/java/LoginPageTest.java
+++ src/test/java/LoginPageTest.java
@@ -1,2 +1,10 @@
 public class LoginPageTest extends BasePageTest {
+    /*@Test (dataProvider = "Locators for tests 41-51", dataProviderClass = LandingPage.class)
+    public void tests_41_42_43_45_47_49_51 (String testNumber, By locator) throws InterruptedException {
+        landingPage.moveToFooter();
+        Assert.assertTrue(landingPage.isDisplayed(locator),
+                "TEST FAILED: Test number is " + testNumber);
+        System.out.println("TEST PASSED. Test number is " + testNumber);
+    }*/
+
 }
